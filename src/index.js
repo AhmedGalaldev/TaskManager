967d270fe1c590acdff32288bfc5c5dae722fbc4
ahmedgalaldev <ahmedgalaldev@gmail.com>
2020-05-22 15:54:25 +0200

    relation between task and user

--- src/index.js
+++ src/index.js
@@ -6,28 +6,27 @@ const taskRouter=require('./routers/task')
 const app=express()
 const port=process.env.port||3000
 
-
-// app.use((req,res,next)=>{
-//         res.status(503).send('the site not avilable now')
-
-// })
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-const jwt=require('jsonwebtoken')
-const myFunction=async()=>{
-    const token =await jwt.sign({_id:"abc123"},'thisismynewcourse',{expiresIn:"7 days"})
 
-    console.log(token);
-
-    const data=jwt.verify(token,'thisismynewcourse')
-    console.log(data);
-    
-    
-}
-myFunction()
 app.listen(port,()=>{
     console.log("The server listen on port "+port);
     
 })
+const User=require('./models/user')
+const Task=require('./models/task')
+
+const main=async()=>{
+    // const task=await Task.findById('5ec7c120906c1d214e369c39')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+    const user=await User.findById('5ec7bfa5824069202325a430')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks);
+    
+    
+}
+
+main()
